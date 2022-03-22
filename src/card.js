e01a22be9db55c0e6d4805b377c02a30cb840ce9
Julio César García Rodríguez <julio07110191@gmail.com>
2022-03-21 22:36:39 -0600

    Done design mobile

--- src/card.js
+++ src/card.js
@@ -3,7 +3,7 @@ class person extends HTMLElement{
         super();
         this.image = this.getAttribute('image');
         this.name = this.getAttribute('name');
-        this.backImage = this.getAttribute('backImg');
+        this.backImage = this.getAttribute('backimg');
         this.attachShadow({ mode: 'open' });
     }
     static get observedAttribute(){
@@ -25,22 +25,82 @@ class person extends HTMLElement{
                 break;
         }
     }
+    getStyle(){
+        return `
+            <style>
+                :host{
+                    width: 95%;
+                    border-radius: 12px;
+                    --text-color: hsl(0, 0%, 100%);
+                    --border-image: #a775f1;
+                }
+                .user{
+                    padding: 15px 32px;
+                    background-image: url(${this.backImage});
+                    background-repeat: no-repeat;
+                    background-position: 93% 10px;
+                    height: 94%;
+                }
+                .user_info{
+                    width: 100%;
+                    display: flex;
+                    align-items: center;
+                    margin-bottom: 10px;
+                    color: var(--text-color);
+                    font-weight: 500;
+                }
+                .user_info img{
+                    width: 40px;
+                    border-radius: 50%;
+                }
+                .user_info p{
+                    margin:  5px 0;
+                }
+                .user_info figure{
+                    width: 40px;
+                    height: 40px;
+                    margin: 0 15px 0 0;
+                    border: 2px solid var(--border-image);
+                    border-radius: 50%;
+                }
+                .user_info-name{
+                    color: var(--text-color);
+                }
+                .user_info-name + p{
+                    font-size: 13px;
+                    opacity: .5;
+                }
+                .user_text-focus{
+                    font-size: 21px;
+                    font-weight: 600;
+                    color: var(--text-color);
+                }
+                .user_text-review{
+                    margin-top: 20px;
+                    color: hsl(0, 0%, 70%);
+                }
+            </style>
+        `;
+    }
     getTemplate(){
         const container = document.createElement('template');
         container.innerHTML = `
-        <section>
-            <div>
+        <section class="user">
+            <div class="user_info">
                 <figure>
                     <img src="${this.image}" alt="Photo of ${this.name}">
                 </figure>
-                <p>${this.name}</p>
-                <p>Verified Graduate</p>
+                <div>
+                    <p class="user_info-name">${this.name}</p>
+                    <p>Verified Graduate</p>
+                </div>
             </div>
             <div>
-                <slot name="principal"><p></p></slot>
-                <slot name="secondary"><p></p></slot>
+                <slot name="principal" class="user_text-focus"><p></p></slot>
+                <slot name="secondary" class="user_text-review"><p></p></slot>
             </div>
-        </section>`;
+        </section>
+        ${this.getStyle()}`;
         return container;
     }
     render(){
